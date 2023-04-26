c03f49595375d4008e66266421efa3584fd5b7a6
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-04-26 15:47:55 -0400

    Cleaning the mess

--- deploy-command.js
+++ deploy-command.js
@@ -1,47 +1,49 @@
-const { REST, Routes } = require('discord.js');
-require('dotenv/config')
-const fs = require('node:fs');
-const path = require('node:path');
+// To update command.
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const foldersPath = path.join(__dirname, '01-commands');
-const commandFolders = fs.readdirSync(foldersPath);
+// const { REST, Routes } = require('discord.js');
+// require('dotenv/config')
+// const fs = require('node:fs');
+// const path = require('node:path');
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
+// const commands = [];
+// // Grab all the command files from the commands directory you created earlier
+// const foldersPath = path.join(__dirname, '01-commands');
+// const commandFolders = fs.readdirSync(foldersPath);
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.TOKEN);
+// for (const folder of commandFolders) {
+// 	// Grab all the command files from the commands directory you created earlier
+// 	const commandsPath = path.join(foldersPath, folder);
+// 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+// 	for (const file of commandFiles) {
+// 		const filePath = path.join(commandsPath, file);
+// 		const command = require(filePath);
+// 		if ('data' in command && 'execute' in command) {
+// 			commands.push(command.data.toJSON());
+// 		} else {
+// 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 		}
+// 	}
+// }
 
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+// // Construct and prepare an instance of the REST module
+// const rest = new REST().setToken(process.env.TOKEN);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-        // Routes.applicationGuildCommands(clientId, guildId),
-		const data = await rest.put(
-            Routes.applicationCommands(process.env.CLIENTID, process.env.GUILDID),
-			{ body: commands },
-		);
+// // and deploy your commands!
+// (async () => {
+// 	try {
+// 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-})();
+// 		// The put method is used to fully refresh all commands in the guild with the current set
+//         // Routes.applicationGuildCommands(clientId, guildId),
+// 		const data = await rest.put(
+//             Routes.applicationCommands(process.env.CLIENTID, process.env.GUILDID),
+// 			{ body: commands },
+// 		);
+
+// 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+// 	} catch (error) {
+// 		// And of course, make sure you catch and log any errors!
+// 		console.error(error);
+// 	}
+// })();
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv/config')
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, IntentsBitField } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, IntentsBitField} = require('discord.js');
 const { Configuration, OpenAIApi} = require('openai');
+
 
 const configuration = new Configuration({
 	apiKey: process.env.API_KEY,
@@ -11,81 +12,55 @@ const openai = new OpenAIApi(configuration);
 
 const client = new Client({ 
 	intents: [
+		IntentsBitField.Flags.GuildMembers,
 		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.DirectMessages,
 		IntentsBitField.Flags.GuildMessages,
 		IntentsBitField.Flags.MessageContent,
 	]
 });
 
-// Event Listenner for command 
-client.cooldowns = new Collection();
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, '01-commands');
-const commandFolders = fs.readdirSync(foldersPath);
+// Command Handler
+//   client.commands = new Collection();
+//   const foldersPath = path.join(__dirname, 'commands');
+//   const commandFolders = fs.readdirSync(foldersPath);
+  
+//   for (const folder of commandFolders) {
+// 	  const commandsPath = path.join(foldersPath, folder);
+// 	  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// 	  for (const file of commandFiles) {
+// 		  const filePath = path.join(commandsPath, file);
+// 		  const command = require(filePath);
+// 		  if ('data' in command && 'execute' in command) {
+// 			  client.commands.set(command.data.name, command);
+// 		  } else {
+// 			  console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 		  }
+// 	  }
+//   }
+  
+// Event Listenner
+  const eventsPath = path.join(__dirname, '02-events');
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+  
+  for (const file of eventFiles) {
+	  const filePath = path.join(eventsPath, file);
+	  const event = require(filePath);
+	  if (event.once) {
+		  client.once(event.name, (...args) => event.execute(...args));
+	  } else {
+		  client.on(event.name, (...args) => event.execute(...args));
+	  }
+  }
+  
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	const { cooldowns } = client;
-
-	if (!cooldowns.has(command.data.name)) {
-		cooldowns.set(command.data.name, new Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.data.name);
-	const defaultCooldownDuration = 3;
-	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-	if (timestamps.has(interaction.user.id)) {
-		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const expiredTimestamp = Math.round(expirationTime / 1000);
-			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
-		}
-	}
-
-	timestamps.set(interaction.user.id, now);
-	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-///////////////////
-
-// client.once(Events.ClientReady, () => {
-// 	console.log('Ready!');
-// });
-// Chat GPT Code
+  // Chat GPT Process
   client.on('messageCreate', async (message) => {
 	if (message.author.bot) return;
 	if (message.channel.id !== process.env.CHANNEL_ID) return;
 	if (!message.content.startsWith('.')) return;
   
-	let conversationLog = [{ role: 'system', content: 'Professionnal chatbot' }];
+	let conversationLog = [{ role: 'system', content: 'Friendly' }];
   
 	try {
 	  await message.channel.sendTyping();
@@ -114,7 +89,7 @@ client.on(Events.InteractionCreate, async interaction => {
 		  console.log(`OPENAI ERR: ${error}`);
 		});
 		// if len of result.data.choices[0] is  than 1950 Caracter
-		if (result.data.choices[0].message.content.length < 1950) {
+		if (result && result.data && result.data.choices && result.data.choices[0].message.content.length < 1950) {
 			message.reply(result.data.choices[0].message);
 		  }
 		  else {
@@ -126,19 +101,5 @@ client.on(Events.InteractionCreate, async interaction => {
 	  console.log(`ERR: ${error}`);
 	}
   });
-
-// Event Listenner
-const eventsPath = path.join(__dirname, '02-events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
 
 client.login(process.env.TOKEN);
